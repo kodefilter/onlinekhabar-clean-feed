20d007d5137f8d83d98b7a64febc707e20c271ce
Pradeep Ghimire <18732242+prrdee@users.noreply.github.com>
2019-07-31 00:44:57 +0300

    Add files via upload

---
+++ contentScript.js
@@ -0,0 +1,10 @@
+console.log("Clean Feed in action")
+
+// Get all the div elements with id containing "-ad" part
+const advertisements = document.querySelectorAll('div[id*="-ad"]')
+
+// Go through each of those and apply a property to hide them
+advertisements.forEach(e => e.style.display = "none")
+
+/* we can also do visibility = hidden */
+/*Pradeep Ghimire*/
