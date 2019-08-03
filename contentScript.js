5296a429ababc6066def50d7b917e1e62a8474ec
Prrdee Geemre <18732242+prrdee@users.noreply.github.com>
2019-08-03 12:38:20 +0300

    Added code to remove roadblock advertisements

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,7 @@
 console.log("Clean Feed in action")
 
 // Get all the div elements with id containing "-ad" part
-const advertisements = document.querySelectorAll('div[id*="-ad"]')
+const advertisements = document.querySelectorAll('div[id*="-ad"], div.ok_roadblock')
 
 // Go through each of those and apply a property to hide them
 advertisements.forEach(e => e.style.display = "none")
